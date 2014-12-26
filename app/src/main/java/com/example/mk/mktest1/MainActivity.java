2d083e90a439164fea2b4b38863b494684b33743
unknown <mkpantira@hotmail.com>
2014-12-26 14:54:04 +0700

    my first commit

---
+++ app/src/main/java/com/example/mk/mktest1/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.mk.mktest1;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+   public void doNothing(View V){
+       //input text
+       EditText myEditText= (EditText) findViewById(R.id.In_name);
+       String myInput= myEditText.getText().toString();
+
+       //change the text in textview
+       TextView myTextView = (TextView) findViewById(R.id.this_kitkat);
+       myTextView.setText(myInput);
+   }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
